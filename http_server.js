65bbbf09d9b108121afc932fc1e9ab7321555921
kagusev <ma01760@yahoo.com>
2022-03-30 14:08:38 -0400

    updated app fro for heroku

--- http_server.js
+++ http_server.js
@@ -1,66 +1,55 @@
-// add http server
-// -----------------------
-// YOUR CODE
+const express = require('express');
+const app = express();
+const low = require('lowdb');
+const fs = require('lowdb/adapters/FileSync');
+const adapter = new fs('db.json');
+const db = low(adapter);
+const cors = require('cors');
 
-var low     = require('lowdb');
-var fs      = require('lowdb/adapters/FileSync');
-var adapter = new fs('db.json');
-var db      = low(adapter);
+// allow cross-origin resource sharing (CORS)
+app.use(cors());
 
-// configure express to serve static files from public directory
-// ------------------------------------------------------------------
-// YOUR CODE
+// data parser - used to parse post data
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// serve static files from public directory
+// -------------------------------------------
+app.use(express.static('public'));
 
 // init the data store
-db.defaults({ posts: []}).write();
+db.defaults({ users: [] }).write();
 
-// list posts
-app.get('/data', function(req, res){     
+let port = process.env.PORT || 3000;
 
-    // YOUR CODE
-
+// return all users
+app.get('/data', function (req, res) {
+    res.send(db.get('users').value());
 });
 
-// ----------------------------------------------------
-// add post - test using:
-//      curl http://localhost:3000/posts/ping/1/false
-// ----------------------------------------------------
-app.get('/posts/:title/:id/:published', function(req, res){
-
-    // YOUR CODE
-
-});
-
-// ----------------------------------------------------
-// filter by published state - test using:
-//      curl http://localhost:3000/published/true
-// ----------------------------------------------------
-app.get('/published/:boolean', function(req, res){
-
-    // YOUR CODE
-
-});
-
-// ----------------------------------------------------
-// update published value - test using:
-//      curl http://localhost:3000/published/1/true
-// ----------------------------------------------------
-app.get('/published/:id/:boolean', function(req, res){
-
-    // YOUR CODE
-
-});
-
-// ----------------------------------------------------
-// delete entry by id - test using:
-//      curl http://localhost:3000/delete/5
-// ----------------------------------------------------
-app.get('/delete/:id/', function(req, res){
-
-    // YOUR CODE
-
+// add user
+app.post('/add', function (req, res) {
+    var user = {
+        'name': req.body.name,
+        'dob': req.body.dob,
+        'email': req.body.email,
+        'username': req.body.username,
+        'password': req.body.password,
+        'phone': req.body.phone,
+        'streetaddress': req.body.streetaddress,
+        'citystatezip': req.body.citystatezip,
+        'latitude': req.body.latitude,
+        'longitude': req.body.longitude,
+        'avatar': req.body.avatar
+    }
+    db.get('users').push(user).write();
+    console.log(db.get('users').value());
+    res.send(db.get('users').value());
 });
 
 // start server
 // -----------------------
-// YOUR CODE
+app.listen(port, function () {
+    console.log(`Running on port ${port}`);
+});
